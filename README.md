798472e729c72dea744d162340fa2f41af6f5dc9
Maximillian Laumeister <max@maxlaumeister.com>
2020-01-09 17:39:06 -0800

    Now in a working state. Closes #1

--- README.md
+++ README.md
@@ -1,14 +1,30 @@
-# "image-rendering: pixelated" CSS Polyfill
+# Pixelated.js - an "image-rendering: pixelated" Polyfill
 
-This is a polyfill for the `image-rendering: pixelated` CSS property, which is currently supported by webkit browsers (Chrome, Safari, Opera), but not by Edge or Firefox.
+`Pixelated.js` is a polyfill that makes it easy to display clean, scaled-up pixel art in the browser.
 
-This polyfill makes it easier to display `img` tags that contain pixel art, without having to scale them beforehand.
+It provides functionality similar to the `image-rendering: pixelated` CSS property, which is currently supported by webkit browsers (Chrome, Safari, Opera), but not by Edge or Firefox.
 
-This polyfill is also a more consistent alternative to using `image-rendering: crisp-edges`, because it guarantees that images will always be scaled using the nearest neighbor algorithm (and, it also works in Edge).
+`Pixelated.js` is also a more consistent alternative to using `image-rendering: crisp-edges`, because it guarantees that images will always be scaled using the nearest neighbor algorithm. [According to the spec, the scaling algorithm for `crisp-edges` is undefined](https://stackoverflow.com/a/20678910/2234742), so `Pixelated.js` makes sure your images look the same across all modern browsers.
 
 ## Usage
 
-(TODO)
+Get started with the following code at the end of your `body`:
+
+    <script src="pixelated.min.js"></script>
+    <script>
+    const elements = document.querySelectorAll('img');
+    PixelatedPolyfill.pixelate(elements);
+    </script>
+
+`Pixelated.js` wraps your `img` element in a `div` with class `pixelated-wrap`. By default, the div is `inline-block`, which gives it similar behavior to an `img` tag.
+
+To make your images `display: block`, use the following style:
+
+    .pixelated-polyfill {
+        display: block;
+    }
+
+You can use this same `pixelated-polyfill` class to add borders, change opacity, etc.
 
 ## Example
 
@@ -16,10 +32,11 @@ This polyfill is also a more consistent alternative to using `image-rendering: c
 
 ## Features
 
-* Responds dynamically to resize events
-* Preserves "right-click, save as" functionality
-* Works on existing `img` elements, no need to change your HTML
+* Designed for all modern browsers (not IE). Tested in Chrome, Firefox, and Edge.
+* Responds gracefully to resize/reflow events, like a real `img` tag
+* Preserves the `img` tag's "right-click, view image" and "save as" functionality
+* Exclusive browser activism feature - seeing blurry pixel art gently reminds IE users that they need to start using a browser that doesn't butcher my code
 
 ## Under the Hood
 
-To render an image, this polyfill wraps the `img` tag in a `div`, then renders the image to a `canvas` element on top of it.
+To render an image, `Pixelated.js` wraps the `img` tag in a `div`, then renders the image to a `canvas` element on top of it. In browsers that support `image-rendering: pixelated` natively, this polyfill still wraps the image in a `div` to ensure consistent CSS behavior, however it lets the native `img` tag show instead of using canvas rendering.
---
+++ pixelated.js
@@ -0,0 +1,129 @@
+(function() {
+    class PixelatedPolyfill {
+        constructor(img) {
+            // TODO: hidpi displays
+            // TODO: lint
+            this.img = img;
+        }
+
+        init() {
+            // Set up div wrappers
+
+            const outerDiv = document.createElement('div');
+            outerDiv.className = "pixelated-polyfill";
+
+            this.div = document.createElement('div');
+            this.div.className = "pixelated-polyfill-inner";
+
+            // Add to DOM
+
+            wrap(this.img, this.div);
+            wrap(this.div, outerDiv);
+            
+            // Polyfill time
+
+            if (!CSS.supports("image-rendering", "pixelated")) {
+                // Set up canvas
+                this.canvas = document.createElement('canvas');
+                this.canvas.style.display = "block";
+                this.canvas.style.width = "100%";
+                this.canvas.style.height = "100%";
+                this.canvas.style.position = "absolute";
+                this.canvas.style.top = "0";
+                this.canvas.style.left = "0";
+                this.canvas.style.pointerEvents = "none";
+
+                this.ctx = this.canvas.getContext("2d");
+
+                this.div.appendChild(this.canvas);
+
+                // Size the canvas and draw
+
+                this.updateCanvas();
+
+                // Hide image, while still allowing it to be clicked on
+
+                this.img.style.opacity = "0";
+
+                // Listen for size changes
+
+                new ResizeObserver(this.updateCanvas.bind(this)).observe(this.div);
+            }
+        }
+
+        updateCanvas() {
+            // Update canvas size
+            const w = this.div.clientWidth;
+            const h = this.div.clientHeight;
+            if (!this.lastSize || this.lastSize.w !== w || this.lastSize.h !== h) {
+                this.canvas.width = w;
+                this.canvas.height = h;
+                this.lastSize = { w, h }
+            }
+            // Redraw
+            this.ctx.save();
+            this.ctx.imageSmoothingEnabled = false;
+            this.ctx.drawImage(this.img, 0, 0, this.canvas.width, this.canvas.height);
+            this.ctx.restore();
+        }
+    }
+
+    function wrap(el, wrapper) {
+        el.parentNode.insertBefore(wrapper, el);
+        wrapper.appendChild(el);
+    }
+
+    PixelatedPolyfill.initialized = false;
+
+    PixelatedPolyfill.pixelate = (elements) => {
+        if (typeof elements[Symbol.iterator] !== 'function') {
+            // If the user passed in a single element, convert it to an array to normalize the code
+            elements = [elements];
+        }
+        
+        if (!PixelatedPolyfill.initialized) {
+            // Set up CSS - insert styles before all other styles in the page, so the user can override it
+
+            PixelatedPolyfill.initialized = true;
+
+            const styles = `
+                .pixelated-polyfill {
+                    display: inline-block;
+                    font-size: 0;
+                }
+                .pixelated-polyfill-inner {
+                    display: inline-block;
+                    position: relative;
+                }
+            `;
+
+            const firstStyleTag = document.querySelector("style, link[type='text/css']");
+
+            const styleSheet = document.createElement("style");
+            styleSheet.type = "text/css";
+            styleSheet.textContent = styles;
+            if (firstStyleTag) {
+                firstStyleTag.parentNode.insertBefore(styleSheet, firstStyleTag);
+            } else {
+                document.head.appendChild(styleSheet);
+            }
+        }
+
+        // Set up elements
+        elements.forEach( img => {
+            if (img instanceof HTMLImageElement) {
+                img.style.imageRendering = "pixelated";
+                const pixelImg = new PixelatedPolyfill(img);
+                if (img.complete) {
+                    pixelImg.init();
+                } else {
+                    img.onload = () => {
+                        pixelImg.init();
+                    };
+                }
+            }
+        });
+    };
+
+    window.PixelatedPolyfill = PixelatedPolyfill;
+})();
